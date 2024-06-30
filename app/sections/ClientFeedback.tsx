00156266b9a896d679a4c264006fb604f5bf9a38
Jervi Sir <gacembekhira@gmail.com>
2024-06-30 15:01:03 +0100

    .

--- app/sections/ClientFeedback.tsx
+++ app/sections/ClientFeedback.tsx
@@ -1,23 +1,6 @@
 'use client'
 import { FC } from 'react';
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Card } from "@/components/ui/card";
-import dynamic from 'next/dynamic';
 import ModalImage from 'react-modal-image';
-
-// Dynamically import the ResponsiveMasonry and Masonry components
-const ResponsiveMasonry = dynamic<ResponsiveMasonryProps>(() => import('react-responsive-masonry').then(mod => mod.ResponsiveMasonry), { ssr: false });
-const Masonry = dynamic<MasonryProps>(() => import('react-responsive-masonry').then(mod => mod.Masonry), { ssr: false });
-
-interface ResponsiveMasonryProps {
-  children: React.ReactNode;
-  columnsCountBreakPoints: { [key: number]: number };
-}
-
-interface MasonryProps {
-  children: React.ReactNode;
-  gutter?: string;
-}
 
 const ClientFeedback: FC = () => {
   const feedbacks = [
@@ -38,20 +21,18 @@ const ClientFeedback: FC = () => {
           Hear what my clients have to say about the work I&apos;ve done for them.
         </p>
       </div>
-      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
-        <Masonry gutter="10px">
-          {feedbacks.map((item, index) => (
-            <div key={index} className="feedback-item">
-              <ModalImage
-                small={item}
-                large={item}
-                alt={`Feedback ${index + 1}`}
-                className="feedback-image"
-              />
-            </div>
-          ))}
-        </Masonry>
-      </ResponsiveMasonry>
+      <div className={'grid-container-masonry'}>
+        {feedbacks.map((item, index) => (
+          <div key={index} className={'grid-item-masonry'}>
+            <ModalImage
+              small={item}
+              large={item}
+              alt={`Feedback ${index + 1}`}
+              className="feedback-image"
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
