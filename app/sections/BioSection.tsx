eb83e2c2cfe1def847962d90edcceb795afc7b59
Jervi Sir <gacembekhira@gmail.com>
2024-07-11 11:39:14 +0100

    changed purple to yellow in bio

--- app/sections/BioSection.tsx
+++ app/sections/BioSection.tsx
@@ -3,20 +3,20 @@ export const BioSection = () => {
     <div className="container px-4 mx-auto max-w-lg rounded-lg shadow-lg ">
       <div className="text-center">
         <p className="text-lg md:text-xl lg:text-2xl text-white leading-relaxed">
-          Hi, I&apos;m <span className="font-bold text-indigo-800">Hika</span>, a video editor proficient in
+          Hi, I&apos;m <span className="font-bold text-amber-500">Hika</span>, a video editor proficient in
         </p>
         <div className="flex justify-center items-center gap-2 mt-4 flex-wrap">
           <img src="https://upload.wikimedia.org/wikipedia/commons/4/40/Adobe_Premiere_Pro_CC_icon.svg" alt="Premiere Pro" style={{ width: 24, height: 24 }} />
-          <span className="font-bold text-indigo-800">Adobe Premiere Pro</span>
+          <span className="font-bold text-amber-500">Adobe Premiere Pro</span>
           <span className="text-white">and</span>
           <img src="https://upload.wikimedia.org/wikipedia/commons/c/cb/Adobe_After_Effects_CC_icon.svg" alt="After Effects" style={{ width: 24, height: 24 }} />
-          <span className="font-bold text-indigo-800">After Effects</span>
+          <span className="font-bold text-amber-500">After Effects</span>
         </div>
         <p className="text-lg md:text-xl lg:text-2xl text-white leading-relaxed mt-4">
           I speak 🗣️
-          <span className="font-bold text-indigo-800"> English</span>, 
-          <span className="font-bold text-indigo-800"> Arabic</span>, and 
-          <span className="font-bold text-indigo-800"> French</span>
+          <span className="font-bold text-amber-500"> English</span>, 
+          <span className="font-bold text-amber-500"> Arabic</span>, and 
+          <span className="font-bold text-amber-500"> French</span>
         </p>
         <p className="text-lg md:text-xl lg:text-2xl text-white leading-relaxed pt-3">
           I blend creativity with technical expertise to craft compelling visual stories.
